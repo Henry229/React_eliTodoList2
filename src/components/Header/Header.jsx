9e7cc430a165d5eb1b7bf8acb320f7a8adc2d05d
Henry Chun <chunhyungjoon@gmail.com>
2022-12-30 13:55:10 +1100

    useContext for darkMode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
 import styles from './Header.module.css';
+import { HiMoon, HiSun } from 'react-icons/hi';
+import { useDarkMode } from '../../context/DarkModeContext';
 
 const Header = ({ filters, filter, onFilterChange }) => {
+  const { darkMode, toggleDarkMode } = useDarkMode();
+
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.button}>
+        {!darkMode && <HiMoon />}
+        {darkMode && <HiSun />}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
